4cae6caa8ef3eab5d33f57e72bb2f6304ad4ab4b
Han Gao <hangao03@gmail.com>
2025-11-11 10:05:55 -0600

    Initialized backend files from Spring Initializr

---
+++ backend/src/main/java/com/example/wardrobe_manager/WardrobeManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.wardrobe_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WardrobeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WardrobeManagerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/wardrobe_manager/WardrobeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wardrobe_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WardrobeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
